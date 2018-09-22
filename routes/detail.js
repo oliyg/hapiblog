5b1328740568b207cbe6a20dbb18bdcd333fb156
OliverYoung <billyangg@qq.com>
2018-09-22 15:35:04 +0800

    [add] 根据 id 获取文章详情

--- routes/detail.js
+++ routes/detail.js
@@ -1,20 +1,32 @@
 const Joi = require('joi');
+const Boom = require('boom');
+const models = require('../models');
 
 const GROUP_NAME = 'detail';
 
 module.exports = [{
   method: 'GET',
-  path: `/${GROUP_NAME}/{date}/{title}`,
+  path: `/${GROUP_NAME}/{date}/{title}/{id}`, // data 和 title 为 SEO 优化
   handler: async (request, reply) => {
-    reply([request.params.date, request.params.title]);
+    const { id: blogId } = request.params;
+    const res = await models.blog.findOne({
+      where: {
+        id: blogId,
+      },
+    });
+    if (!res) {
+      reply(Boom.notFound('资源请求失败'));
+    }
+    reply(res);
   },
   config: {
     tags: ['api', GROUP_NAME],
-    description: '根据日期和标题获取文章详情',
+    description: '根据 id 获取文章详情',
     validate: {
       params: {
         date: Joi.string().regex(/^\d{4}-\d{1,2}-\d{1,2}$/).required(), // eg: 2018-12-12 2017-1-1
         title: Joi.string().min(1).required(),
+        id: Joi.number().min(1).required(),
       },
     },
     auth: false,
